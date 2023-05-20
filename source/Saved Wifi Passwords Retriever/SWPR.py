884921ff17975b3e11e75087007a28fbd173ecb3
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2023-05-20 21:25:36 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	source/USB Device Logs/UDL.py

--- source/Saved Wifi Passwords Retriever/SWPR.py
+++ source/Saved Wifi Passwords Retriever/SWPR.py
@@ -1,5 +1,6 @@
 #Saved Wifi Password Retrever or SWPR
 import subprocess
 
+
 cmd = 'for /f "skip=9 tokens=1,2 delims=:" %i in (\'netsh wlan show profiles\') do @if "%j" NEQ "" (echo SSID: %j & netsh wlan show profiles %j key=clear | findstr "Key Content") >> wifipasswords.txt'
 subprocess.run(cmd, shell=True)

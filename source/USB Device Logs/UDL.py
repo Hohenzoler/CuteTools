e723aa99cb85811d96d4eecb2b8a4c8bc18d2dfa
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2023-05-29 20:01:25 +0200

    v.0.0.1

--- source/USB Device Logs/UDL.py
+++ source/USB Device Logs/UDL.py
@@ -1,1 +1,15 @@
 #USB Device Logs or UDL
+import subprocess
+def run_powershell_command(command):
+    process = subprocess.Popen(["powershell", "-Command", command], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    return output.decode('utf-8'), error.decode('utf-8')
+
+command = "Get-PnpDevice"
+output, error = run_powershell_command(command)
+if output:
+    print("Output:")
+    print(output)
+if error:
+    print("Error:")
+    print(error)
